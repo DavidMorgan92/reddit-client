4905671c4bd7fa8eab8698ed4534a9adb1961b8a
David Morgan <david.morgan@europe.com>
2021-12-26 02:48:25 +0000

    Remove error catches in reddit API

--- src/api/reddit.js
+++ src/api/reddit.js
@@ -5,37 +5,29 @@ const routes = {
 
 const reddit = {
 	async getTopSubreddits() {
-		try {
-			const response = await fetch(routes.popularSubreddits());
-			const json = await response.json();
-			return json.data.children.map(child => ({
-				id: child.data.id,
-				icon: child.data.icon_img,
-				name: child.data.display_name,
-			}));
-		} catch (error) {
-			return null;
-		}
+		const response = await fetch(routes.popularSubreddits());
+		const json = await response.json();
+		return json.data.children.map(child => ({
+			id: child.data.id,
+			icon: child.data.icon_img,
+			name: child.data.display_name,
+		}));
 	},
 
 	async getPostsBySearchTerm(searchTerm) {
-		try {
-			const response = await fetch(routes.postsBySearchTerm(searchTerm));
-			const json = await response.json();
-			return json.data.children.map(child => ({
-				id: child.data.id,
-				title: child.data.title,
-				content: '',
-				author: child.data.author,
-				age: '',
-				numComments: child.data.num_comments,
-				upvotes: child.data.score,
-				userUpvoted: false,
-				userDownvoted: false,
-			}));
-		} catch (error) {
-			return null;
-		}
+		const response = await fetch(routes.postsBySearchTerm(searchTerm));
+		const json = await response.json();
+		return json.data.children.map(child => ({
+			id: child.data.id,
+			title: child.data.title,
+			content: '',
+			author: child.data.author,
+			age: '',
+			numComments: child.data.num_comments,
+			upvotes: child.data.score,
+			userUpvoted: false,
+			userDownvoted: false,
+		}));
 	},
 };
 
