1774199201698021df16a1cfb791c46f93cd91e6
David Morgan <david.morgan@europe.com>
2021-12-26 02:36:13 +0000

    Handle network errors gracefully

--- src/api/reddit.js
+++ src/api/reddit.js
@@ -5,29 +5,37 @@ const routes = {
 
 const reddit = {
 	async getTopSubreddits() {
-		const response = await fetch(routes.popularSubreddits());
-		const json = await response.json();
-		return json.data.children.map(child => ({
-			id: child.data.id,
-			icon: child.data.icon_img,
-			name: child.data.display_name,
-		}));
+		try {
+			const response = await fetch(routes.popularSubreddits());
+			const json = await response.json();
+			return json.data.children.map(child => ({
+				id: child.data.id,
+				icon: child.data.icon_img,
+				name: child.data.display_name,
+			}));
+		} catch (error) {
+			return null;
+		}
 	},
 
 	async getPostsBySearchTerm(searchTerm) {
-		const response = await fetch(routes.postsBySearchTerm(searchTerm));
-		const json = await response.json();
-		return json.data.children.map(child => ({
-			id: child.data.id,
-			title: child.data.title,
-			content: '',
-			author: child.data.author,
-			age: '',
-			numComments: child.data.num_comments,
-			upvotes: child.data.score,
-			userUpvoted: false,
-			userDownvoted: false,
-		}));
+		try {
+			const response = await fetch(routes.postsBySearchTerm(searchTerm));
+			const json = await response.json();
+			return json.data.children.map(child => ({
+				id: child.data.id,
+				title: child.data.title,
+				content: '',
+				author: child.data.author,
+				age: '',
+				numComments: child.data.num_comments,
+				upvotes: child.data.score,
+				userUpvoted: false,
+				userDownvoted: false,
+			}));
+		} catch (error) {
+			return null;
+		}
 	},
 };
 
--- src/api/reddit.test.js
+++ src/api/reddit.test.js
@@ -1,14 +1,31 @@
 import reddit from './reddit';
-import { mockSubreddits, mockPosts } from './test/server-handlers.js';
+import server from './test/server';
+import { mockSubreddits, mockPosts, subredditsHandlerException, searchHandlerException } from './test/server-handlers';
 
 describe('Reddit API', () => {
-	it('loads top subreddits', async () => {
-		const subreddits = await reddit.getTopSubreddits();
-		expect(subreddits.length).toEqual(mockSubreddits.data.children.length);
+	describe('getTopSubreddits', () => {
+		it('loads top subreddits', async () => {
+			const subreddits = await reddit.getTopSubreddits();
+			expect(subreddits.length).toEqual(mockSubreddits.data.children.length);
+		});
+	
+		it('handles network error gracefully', async () => {
+			server.use(subredditsHandlerException);
+			const subreddits = await reddit.getTopSubreddits();
+			expect(subreddits).toBe(null);
+		});
 	});
 
-	it('gets post by search term', async () => {
-		const posts = await reddit.getPostsBySearchTerm('trees');
-		expect(posts.length).toEqual(mockPosts.data.children.length);
+	describe('getPostsBySearchTerm', () => {
+		it('gets posts by search term', async () => {
+			const posts = await reddit.getPostsBySearchTerm('');
+			expect(posts.length).toEqual(mockPosts.data.children.length);
+		});
+	
+		it('handles network error gracefully', async () => {
+			server.use(searchHandlerException);
+			const subreddits = await reddit.getPostsBySearchTerm('');
+			expect(subreddits).toBe(null);
+		});
 	});
 });
