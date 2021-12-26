7b896a4cf404b3ec2b80f1457fb97b163b77ef8d
David Morgan <david.morgan@europe.com>
2021-12-26 00:47:57 +0000

    Change test server export

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import { server } from './api/test/server.js';
+import server from './api/test/server.js';
 
 configure({ adapter: new Adapter() });
 
