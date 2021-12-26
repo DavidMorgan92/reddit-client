5f52d1382028d6cfd87504faac779a49d3ae31c6
David Morgan <david.morgan@europe.com>
2021-12-26 02:55:30 +0000

    Remove mock server and API tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,4 @@
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import server from './api/test/server.js';
 
 configure({ adapter: new Adapter() });
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
