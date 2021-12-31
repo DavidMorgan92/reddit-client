3ecb5dff30120efeaf352450c55309ce3c7b899f
David Morgan <david.morgan@europe.com>
2021-12-31 18:43:36 +0000

    Add footer for Reddit icon attribution

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,8 @@ import store from '../../store/store';
 import Header from '../Header/Header';
 import PostList from '../PostList/PostList';
 import SubredditList from '../SubredditList/SubredditList';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
 	return (
@@ -14,6 +16,15 @@ function App() {
 				<PostList />
 				<SubredditList />
 			</div>
+			<footer>
+				<small>
+					Reddit icon provided by&nbsp;
+					<a href='https://iconscout.com/contributors/pocike' target='_blank' rel='noopener noreferrer'>
+						"Those Icons"&nbsp;
+						<sup><FontAwesomeIcon icon={faExternalLinkAlt} /></sup>
+					</a>
+				</small>
+			</footer>
 		</Provider>
 	);
 }
