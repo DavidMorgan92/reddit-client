4e5f86120bb5f6ebc40eab69e2a98e11af26b066
David Morgan <david.morgan@europe.com>
2022-01-02 08:37:45 +0000

    Responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Header.css';
 import Search from '../Search/Search';
+import useBreakpoints from '../../util/useBreakpoints';
 
 function Header() {
+	const { isXs } = useBreakpoints();
+
 	return (
 		<header>
 			<img src={'/reddit-168.svg'} alt='Logo' />
-			<h1><a href='/'>Reddit Client</a></h1>
+			{isXs || <h1><a href='/'>Reddit Client</a></h1>}
 			<Search />
 		</header>
 	);
