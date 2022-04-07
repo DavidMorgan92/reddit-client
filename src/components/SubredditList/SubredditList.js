2ad4494f7b2f0d2488aca87f4cdc97d4e23a8470
David Morgan <david.morgan@europe.com>
2022-04-07 19:39:01 +0100

    Transition subreddit list opacity

--- src/components/SubredditList/SubredditList.js
+++ src/components/SubredditList/SubredditList.js
@@ -1,13 +1,27 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown, faCaretRight, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import {
+	faCaretDown,
+	faCaretRight,
+	faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 import './SubredditList.css';
 import Subreddit from '../Subreddit/Subreddit';
-import { loadSubreddits, selectFailedToLoadSubreddits, selectIsLoadingSubreddits, selectSelectedSubreddit, selectSubreddits, selectSubredditsListOpen, setSelectedSubreddit, setSubredditsListOpen } from '../../store/subredditsSlice';
+import {
+	loadSubreddits,
+	selectFailedToLoadSubreddits,
+	selectIsLoadingSubreddits,
+	selectSelectedSubreddit,
+	selectSubreddits,
+	selectSubredditsListOpen,
+	setSelectedSubreddit,
+	setSubredditsListOpen,
+} from '../../store/subredditsSlice';
 import { loadPosts } from '../../store/postsSlice';
 import { selectSearchTerm } from '../../store/searchSlice';
 import useBreakpoints from '../../util/useBreakpoints';
+import { useSpring, animated } from 'react-spring';
 
 function SubredditList() {
 	const dispatch = useDispatch();
@@ -20,7 +34,7 @@ function SubredditList() {
 
 	const handleSubredditClick = subreddit => {
 		dispatch(setSelectedSubreddit(subreddit.id));
-		dispatch(loadPosts({subredditName: subreddit.name, searchTerm}));
+		dispatch(loadPosts({ subredditName: subreddit.name, searchTerm }));
 	};
 
 	const handleOpenerClick = () => {
@@ -33,32 +47,9 @@ function SubredditList() {
 
 	const { isXs, isSm, isMd, isLg } = useBreakpoints();
 
-	let children;
-
-	if (subredditsListOpen || isMd || isLg) {
-		children = <FontAwesomeIcon className='SubredditList__Spinner' icon={faSpinner} spin />;
-
-		if (!isLoadingSubreddits) {
-			if (failedToLoadSubreddits) {
-				children = <div className='error-message'>Error occurred getting subreddits</div>;
-			} else {
-				children = (
-					<div className='SubredditList__Container'>
-						{
-							subreddits.map(subreddit => (
-								<Subreddit
-									key={subreddit.id}
-									subreddit={subreddit}
-									onClick={handleSubredditClick}
-									isSelected={selectedSubreddit?.id === subreddit.id}
-								/>
-							))
-						}
-					</div>
-				);
-			}
-		}
-	}
+	let spring = false;
+	let subredditList = null;
+	let staticDiv = null;
 
 	let opener;
 
@@ -67,16 +58,63 @@ function SubredditList() {
 
 		opener = (
 			<div className='SubredditList__Opener' onClick={handleOpenerClick}>
-				<FontAwesomeIcon className='SubredditList__OpenerIcon' icon={caretIcon} />
+				<FontAwesomeIcon
+					className='SubredditList__OpenerIcon'
+					icon={caretIcon}
+				/>
 				<h3>Subreddits</h3>
 			</div>
 		);
 	}
 
+	if (subredditsListOpen || isMd || isLg) {
+		staticDiv = (
+			<FontAwesomeIcon
+				className='SubredditList__Spinner'
+				icon={faSpinner}
+				spin
+			/>
+		);
+
+		if (!isLoadingSubreddits) {
+			spring = true;
+
+			if (failedToLoadSubreddits) {
+				staticDiv = (
+					<div className='error-message'>
+						Error occurred getting subreddits
+					</div>
+				);
+			} else {
+				staticDiv = null;
+				subredditList = (
+					<div className='SubredditList__Container'>
+						{subreddits.map(subreddit => (
+							<Subreddit
+								key={subreddit.id}
+								subreddit={subreddit}
+								onClick={handleSubredditClick}
+								isSelected={selectedSubreddit?.id === subreddit.id}
+							/>
+						))}
+					</div>
+				);
+			}
+		}
+	}
+
+	const styles = useSpring({ opacity: spring ? 1 : 0 });
+
+	const animatedDiv = (
+		<animated.div style={styles}>
+			{subredditList}
+		</animated.div>
+	);
+
 	return (
 		<div className='SubredditList'>
 			{opener}
-			{children}
+			{staticDiv || animatedDiv}
 		</div>
 	);
 }
